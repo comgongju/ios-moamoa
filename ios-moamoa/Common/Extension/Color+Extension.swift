ee5f7295e873e5ce26cf5be01603e0b3607ca321
soojung-shin <ssj15987@naver.com>
2020-07-17 17:35:21 +0900

    [#2] Colors set 정의  - Colors asset catalog 추가  - 각 색상 Color static 변수로 정의: 네이밍 디자인 가이드와 동일

---
+++ ios-moamoa/Common/Extension/Color+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Extension.swift
+//  ios-moamoa
+//
+//  Created by Soojung Shin on 2020/07/12.
+//  Copyright © 2020 comgongju. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let mainColor: Color = Color("mainColor")
+    static let pinkredTextColor: Color = Color("pinkredTextColor")
+    static let defaultTextColor: Color = Color("defaultTextColor")
+    static let layerSectionColor: Color = Color("layerSectionColor")
+    static let subTextColor: Color = Color("subTextColor")
+    static let ssubTextColor: Color = Color("ssubTextColor")
+    static let subColor: Color = Color("subColor")
+}
